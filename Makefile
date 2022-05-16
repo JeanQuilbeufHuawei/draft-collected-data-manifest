5f3d41911f5ff0c8e5d1c8206022211b68964d75
Jean Quilbeuf <jean.quilbeuf@huawei.com>
2022-05-16 08:52:57 +0100

    Update template to last version and build script

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 all:
 	cd builder; python3 build_data_manifest_draft.py
-	LOCALE="EN_us.utf8" xml2rfc --v2 draft-claise-opsawg-collected-data-manifest-??.xml
+	LOCALE="EN_us.utf8" xml2rfc --v2 $(shell ls draft-claise-opsawg-collected-data-manifest-??.xml | sort | tail -n 1)
 
--- builder/build_data_manifest_draft.py
+++ builder/build_data_manifest_draft.py
@@ -82,7 +82,7 @@ def draft_content():
 
 
 if __name__ == '__main__':
-    output = os.path.join(os.path.dirname(BUILDER_DIR), "draft-claise-opsawg-collected-data-manifest-01.xml")
+    output = os.path.join(os.path.dirname(BUILDER_DIR), "draft-claise-opsawg-collected-data-manifest-02.xml")
     draft_text = env.get_template("draft-claise-opsawg-collected-data-manifest.xml")
     with open(output, 'w') as xml_generated:
         xml_generated.write(draft_text.render(**draft_content()))
